0b81f13b25eb0e6ee7fb023927cc9245f7990332
ahmedsamir96m <ahmedsamir96m@gmail.com>
2020-09-18 14:53:29 +0200

    set up the basic api call

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import "./App.css";
 import WeatherCard from "./Components/WeatherCard/WeatherCard";
 
 function App() {
+  const weatherData = async () => {
+    const apiResponse = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=california&units=metric&appid=043a835a34904ba1bd34803d928b7700"
+    );
+    const jsonData = await apiResponse
+      .json()
+      .then((resp) => console.log(resp.name));
+    return jsonData;
+  };
+
+  weatherData();
+
   return (
     <div className="App">
       <WeatherCard
