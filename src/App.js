6c5ec36f6cd20bb7b74e654921dc4c309d826958
ahmedsamir96m <ahmedsamir96m@gmail.com>
2020-09-21 16:35:57 +0200

    moved data handling and api handling to the controller

--- src/App.js
+++ src/App.js
@@ -1,73 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import WeatherCard from "./Components/WeatherCard/WeatherCard";
+import WeatherController from "./Components/WeatherController/WeatherController";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("tokyo");
-  const [temperature, setTemperature] = useState("");
-  const [weatherCondition, setWeatherCondition] = useState("");
-  const [city, setCity] = useState("");
-  const [country, setCountry] = useState("");
-  const [iconImage, setIconImage] = useState("");
-
-  const weatherData = async (query) => {
-    const apiResponse = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=043a835a34904ba1bd34803d928b7700`
-    );
-    const jsonData = await apiResponse.json();
-    return jsonData;
-  };
-
-  const handleCitySearch = (e) => {
-    e.preventDefault();
-    weatherData(searchQuery).then((resp) => {
-      setCity(resp.name);
-      setCountry(resp.sys.country);
-      setWeatherCondition(resp.weather[0].main);
-      setIconImage(resp.weather[0].icon);
-      setTemperature(resp.main.temp);
-    });
-  };
-
-  useEffect(() => {
-    weatherData(searchQuery).then((resp) => {
-      setCity(resp.name);
-      setCountry(resp.sys.country);
-      setWeatherCondition(resp.weather[0].main);
-      setIconImage(resp.weather[0].icon);
-      setTemperature(resp.main.temp);
-    });
-  }, []);
-
   return (
     <div className="App">
-      <form action="">
-        <input
-          type="text"
-          name="city"
-          id="weatherCity"
-          placeholder="Enter city name"
-          onChange={(e) => {
-            setSearchQuery(e.target.value);
-          }}
-        />
-        <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            handleCitySearch(e);
-          }}
-        >
-          Search
-        </button>
-      </form>
-      <WeatherCard
-        temperature={temperature}
-        weatherCondition={weatherCondition}
-        city={city}
-        country={country}
-        iconImage={iconImage}
-      />
+      <WeatherController location="tokyo" />
     </div>
   );
 }
